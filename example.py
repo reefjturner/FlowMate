15d1ab708ade04a09140b7e517280930ae2b546b
Reef Turner <reef.turner@sydney.edu.au>
2024-11-08 20:21:18 +1100

    Add files via upload

---
+++ example.py
@@ -0,0 +1,20 @@
+'''
+Onion
+'''
+
+import FlowMate
+import numpy as np
+
+# define a function which returns dq/dt, dp/dt from Hamiltons equations
+def X(q,p,k):
+    return q*p, -k
+
+# define the phase space
+q = np.linspace(-5, 5, 21)
+p = np.linspace(-5, 5, 31)
+PS = np.meshgrid(q, p)
+
+# plot
+FlowMate.plot_phase_portrait(X, PS, args=[3])
+
+
